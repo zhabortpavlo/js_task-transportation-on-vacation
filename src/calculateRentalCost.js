6715b36ea8fa57cd447f23f957a325e7e1a814b8
Pavlo Zhabort <zhabortp@gmail.com>
2025-06-16 15:39:43 +0300

    done task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentPerDay = 40;
+  const shortDiscount = 20;
+  const longDiscount = 50;
+  const totalCost = days * rentPerDay;
+
+  if (days >= 7) {
+    return totalCost - longDiscount;
+  } else if (days >= 3) {
+    return totalCost - shortDiscount;
+  } else {
+    return totalCost;
+  }
 }
 
 module.exports = calculateRentalCost;
